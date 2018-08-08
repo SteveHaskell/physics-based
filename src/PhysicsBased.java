363c48cb0daf1e6d61346cf82c911976d45fbd91
Steve Haskell <steven.haskell.is@gmail.com>
2018-08-08 14:46:59 -0700

    second commit, no real content yet

---
+++ src/PhysicsBased.java
@@ -0,0 +1,14 @@
+
+
+
+/*
+ * main class just wraps sub classes, no work should be done here.
+ */
+
+public class PhysicsBased {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+  }
+}
